d083e774e4f06a60ba807ae2595e20d9cb20766d
Soohan <soohancho@icloud.com>
2020-11-10 12:15:20 +0900

    DNS

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://54.180.108.213/api",
+    baseURL:"https://api.trantranslate.com/api",
     headers: {'Access-Control-Allow-Origin': '*',}
 })
 
